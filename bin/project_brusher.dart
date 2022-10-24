856c3659aa4698aa67c9d050d8ca7fdb137acf7e
nullskill <22912338+nullskill@users.noreply.github.com>
2022-10-24 19:32:18 +0300

    Add runZonedGuarded error handling

--- bin/project_brusher.dart
+++ bin/project_brusher.dart
@@ -1,5 +1,16 @@
+import 'dart:async';
+
 import 'package:project_brusher/project_brusher.dart' as project_brusher;
 
 void main(List<String> arguments) {
-  project_brusher.main(arguments);
+  runZonedGuarded(
+    () => project_brusher.main(arguments),
+    (error, stack) {
+      if (error is FormatException) {
+        print(error.message);
+      } else {
+        print(error.toString());
+      }
+    },
+  );
 }
